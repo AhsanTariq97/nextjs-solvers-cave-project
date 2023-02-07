2ebc5c197c2d4e7633b37d6d27a31a9811530de2
AhsanTariq97 <101797964+AhsanTariq97@users.noreply.github.com>
2023-02-07 12:27:18 +0500

    Update Button.jsx

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Button = ({ onClick,type, bgColor, text, pl, icon }) => {
   return (
-    <button onClick={onClick} type={type} className={`px-6 py-2 bg-[${bgColor}] font-normal text-base rounded-sm text-white border-2 border-[#58ae57] hover:bg-white hover:text-black`}>{text}<span className={`${pl}`}>{icon && <FontAwesomeIcon icon={icon} />}</span></button>
+    <button onClick={onClick} type={type} className={`px-6 py-2 bg-[${bgColor}] font-normal text-base rounded-sm text-white border-2 border-[#58ae57] outline-none hover:bg-white hover:text-black`}>{text}<span className={`${pl}`}>{icon && <FontAwesomeIcon icon={icon} />}</span></button>
   )
 }
 
